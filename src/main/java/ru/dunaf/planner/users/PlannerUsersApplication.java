e33687bd479637803013db97091e10630d1891f1
Alexandra_Dunaf <sasha-kalugina@yandex.ru>
2023-09-29 21:18:18 +0400

    transfer User of their monolithic application

---
+++ src/main/java/ru/dunaf/planner/users/PlannerUsersApplication.java
@@ -0,0 +1,19 @@
+package ru.dunaf.planner.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@ComponentScan(basePackages = {"ru.dunaf.planner"})
+@EnableJpaRepositories(basePackages = {"ru.dunaf.planner.users"})
+public class PlannerUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlannerUsersApplication.class, args);
+    }
+
+}
